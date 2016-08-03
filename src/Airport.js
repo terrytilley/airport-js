1a17a144faa1f80f6f93a598be9fea44a107530d
sus111 <sgraham535@gmail.com>
2016-08-03 15:07:44 +0100

    isStormy

--- src/Airport.js
+++ src/Airport.js
@@ -10,5 +10,12 @@ Airport.prototype.clearForLanding = function(plane){
 };
 
 Airport.prototype.clearForTakeOff = function(plane){
+  if(this.isStormy()) {
+    throw new Error('cannot takeoff during storm');
+  };
   this._hangar = [];
+};
+
+Airport.prototype.isStormy = function(){
+  return false;
 };
